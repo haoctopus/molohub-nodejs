57b3149adbc76f7f568fe5fb03315907543fb45d
SchumyHao <schumyhaojl@126.com>
2019-06-10 15:58:04 +0800

    Update demo
    Signed-off-by: SchumyHao <schumyhaojl@126.com>

--- example/demo/index.js
+++ example/demo/index.js
@@ -1,8 +1,7 @@
-var molo_hub_client_1 = require("../../build/molo_hub_client");
-var molo_client_app_1 = require("../../build/molo_client_app");
-var config_json_1 = require("./config.json");
-var client = new molo_hub_client_1.MolohubClient(config_json_1.rhost, config_json_1.rport, config_json_1.lhost, config_json_1.lport);
-var app = new molo_client_app_1.MoloClientApp(client);
+var molo = require("../../build/index");
+var config = require("./config.json");
+var client = new molo.Client(config.rhost, config.rport, config.lhost, config.lport);
+var app = new molo.App(client);
 
 client.on("newSeed", (seed) => {
     console.log("new seed got " + seed);
